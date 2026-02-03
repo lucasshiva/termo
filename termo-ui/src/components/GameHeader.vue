ccc0d57ce1d2bc7e205c0ad5b6a35ff1a0d66d7f
Lucas Silva <silva.lucasdev@gmail.com>
2026-02-03 19:47:42 -0300

    fix: set settings placeholder as disabled

--- termo-ui/src/components/GameHeader.vue
+++ termo-ui/src/components/GameHeader.vue
@@ -1,14 +1,14 @@
 <script lang="ts" setup>
-import { LucideSettings } from 'lucide-vue-next';
-import HelpDialogButton from './HelpDialogButton.vue';
-import Button from './ui/button/Button.vue';
+import { LucideSettings } from 'lucide-vue-next'
+import HelpDialogButton from './HelpDialogButton.vue'
+import Button from './ui/button/Button.vue'
 </script>
 
 <template>
   <div class="m-4 flex items-center justify-between select-none text-center">
     <HelpDialogButton />
     <h1 class="text-3xl font-medium">TERMO</h1>
-    <Button variant="outline">
+    <Button disabled variant="outline">
       <LucideSettings />
     </Button>
   </div>
